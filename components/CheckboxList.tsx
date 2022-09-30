afab3e63c487f223745723b56478d9a69a58664c
Ken Nersisyan <karapet.nersisyan@yahoo.com>
2022-09-30 16:44:15 -0700

    seperate handleChange functions

--- components/CheckboxList.tsx
+++ components/CheckboxList.tsx
@@ -1,46 +1,45 @@
-import { ChangeEvent, useState } from 'react';
+import { useState } from 'react';
 
+// Components
 import Checkbox from './Checkbox';
 
+// Interfaces
 interface CheckboxOptions {
   name: string;
   isChecked: boolean;
 }
 
-const CheckboxList = () => {
-  const [options, setOptions] = useState<CheckboxOptions[]>([
-    {
-      name: 'Include Uppercase Letters',
-      isChecked: false,
-    },
-    {
-      name: 'Include Lowercase Letters',
-      isChecked: false,
-    },
-    {
-      name: 'Include Numbers',
-      isChecked: false,
-    },
-    {
-      name: 'Include Symbols',
-      isChecked: false,
-    },
-  ]);
+const initialValue: CheckboxOptions[] = [
+  {
+    name: 'Include Uppercase Letters',
+    isChecked: true,
+  },
+  {
+    name: 'Include Lowercase Letters',
+    isChecked: true,
+  },
+  {
+    name: 'Include Numbers',
+    isChecked: false,
+  },
+  {
+    name: 'Include Symbols',
+    isChecked: false,
+  },
+];
 
-  console.log(options);
+const CheckboxList = () => {
+  const [options, setOptions] = useState<CheckboxOptions[]>(initialValue);
+
+  const handleChange = (item: CheckboxOptions) => {
+    setOptions(options.map((opt) => (opt.name === item.name ? item : opt)));
+  };
 
   return (
     <ul className="my-8 space-y-4">
       {options.map(({ name, isChecked }) => (
         <li key={name} className="flex items-center">
-          <Checkbox
-            obj={{ name, isChecked }}
-            handleChange={(item) => {
-              setOptions(
-                options.map((opt) => (opt.name === item.name ? item : opt))
-              );
-            }}
-          />
+          <Checkbox obj={{ name, isChecked }} handleChange={handleChange} />
           <span className="w-full text-secondary-light">{name}</span>
         </li>
       ))}
